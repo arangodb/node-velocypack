ab8cdbd6b87590624eed73ebabeeb8fa3a61a237
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-12-07 15:25:31 +0100

    add test for custom type handler

--- test.js
+++ test.js
@@ -14,3 +14,5 @@ console.log("buffer:");
 console.log(test_vpack_buffer);
 console.log("js object:");
 console.log(vpack.decode(test_vpack_buffer));
+
+vpack.decode(new Buffer([11,37,3,49,70,112,111,116,97,116,97,51,243,23,59,10,0,0,0,0,0,50,75,95,85,76,79,49,100,45,79,45,45,45,11,3,21]));
